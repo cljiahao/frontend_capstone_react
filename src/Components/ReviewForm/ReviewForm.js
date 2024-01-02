68dc4cf6e2488c398171ad423b5d41d555fd1913
cljiahao <cljiahao@hotmail.com>
2024-01-02 03:24:51 -0500

    Added Review

---
+++ src/Components/ReviewForm/ReviewForm.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+
+const ReviewForm = () => {
+    return(
+        <div style={{width: '100%', height: '100%', position: 'relative', background: 'white'}}>
+    <div style={{width: 1500, height: 150, left: 150, top: 70, position: 'absolute'}}>
+        <div style={{width: 1500, height: 150, left: 0, top: 0, position: 'absolute'}}>
+            <div style={{width: 1500, height: 75, left: 0, top: 75, position: 'absolute', background: '#D9D9D9'}} />
+            <div style={{width: 200, height: 75, left: 0, top: 0, position: 'absolute', color: 'black', fontSize: 40, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Reviews</div>
+        </div>
+        <div style={{width: 1450, height: 50, left: 40, top: 87, position: 'absolute'}}>
+            <div style={{width: 150, height: 50, left: 0, top: 0, position: 'absolute', color: 'black', fontSize: 23, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>S/N</div>
+            <div style={{width: 400, height: 50, left: 150, top: 0, position: 'absolute', color: 'black', fontSize: 23, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Doctor Name</div>
+            <div style={{width: 300, height: 50, left: 550, top: 0, position: 'absolute', color: 'black', fontSize: 23, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Doctor Specialty</div>
+            <div style={{width: 300, height: 50, left: 850, top: 0, position: 'absolute', color: 'black', fontSize: 23, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Provide Review</div>
+            <div style={{width: 300, height: 50, left: 1150, top: 0, position: 'absolute', color: 'black', fontSize: 23, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Review Given</div>
+        </div>
+    </div>
+    <div style={{width: 1090, height: 50, left: 200, top: 250, position: 'absolute'}}>
+        <div style={{width: 100, height: 50, left: 0, top: 0, position: 'absolute', color: 'black', fontSize: 23, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>1</div>
+        <div style={{width: 400, height: 50, left: 140, top: 0, position: 'absolute', color: 'black', fontSize: 23, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Dr. John Doe</div>
+        <div style={{width: 300, height: 50, left: 540, top: 0, position: 'absolute', color: 'black', fontSize: 23, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Cardiology</div>
+        <div style={{width: 250, height: 50, left: 840, top: 0, position: 'absolute'}}>
+            <div style={{width: 250, height: 50, left: 0, top: 0, position: 'absolute', background: '#4591EA', borderRadius: 10}} />
+            <div style={{width: 150, height: 50, left: 50, top: 0, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 23, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Give Review</div>
+        </div>
+    </div>
+    <div style={{width: 1090, height: 50, left: 200, top: 330, position: 'absolute'}}>
+        <div style={{width: 100, height: 50, left: 0, top: 0, position: 'absolute', color: 'black', fontSize: 23, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>2</div>
+        <div style={{width: 400, height: 50, left: 140, top: 0, position: 'absolute', color: 'black', fontSize: 23, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Dr. Jane Smith</div>
+        <div style={{width: 300, height: 50, left: 540, top: 0, position: 'absolute', color: 'black', fontSize: 23, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Dermatology</div>
+        <div style={{width: 250, height: 50, left: 840, top: 0, position: 'absolute'}}>
+            <div style={{width: 250, height: 50, left: 0, top: 0, position: 'absolute', background: '#4591EA', borderRadius: 10}} />
+            <div style={{width: 150, height: 50, left: 50, top: 0, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 23, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Give Review</div>
+        </div>
+    </div>
+</div>
+    )
+}
