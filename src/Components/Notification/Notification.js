c5b8c39977766db55cd242f362ab078ae47d64d7
cljiahao <cljiahao@hotmail.com>
2024-01-02 01:58:01 -0500

    Updated notifications

--- src/Components/Notification/Notification.js
+++ src/Components/Notification/Notification.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from '../NavBar/NavBar';
+import "./Notification.css";
 
 const Notification = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
   const [username, setUsername] = useState("");
   const [doctorData, setDoctorData] = useState(null);
-  const [appointmentData, setAppointmentData] = useState("");
+  const [appointmentData, setAppointmentData] = useState(null);
 
   useEffect(() => {
     const storedUsername = sessionStorage.getItem('email');
@@ -29,14 +30,18 @@ const Notification = ({ children }) => {
     <div>
       <Navbar ></Navbar>
       {children}
-      {isLoggedIn && appointmentData && (
+      {isLoggedIn && (
         <>
           <div className="appointment-card">
             <div className="appointment-card__content">
               <h3 className="appointment-card__title">Appointment Details</h3>
               <p className="appointment-card__message">
-                <strong>Doctor: Dr. Jiao Yang</strong> {doctorData?.name}
-                Speciality: Dentist
+                <p><strong>Doctor: </strong> Dr. Jiao Yang</p>
+                <p><strong>Specialty: </strong> Dentist</p>
+                <p><strong>Name: </strong> Test</p>
+                <p><strong>Phone Number: </strong> 1234567890</p>
+                <p><strong>Date: </strong> 2024-02-09</p>
+                <p><strong>Time: </strong> 17:20 PM</p>
               </p>
             </div>
           </div>
